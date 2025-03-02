d3807396e69882bafa6e2395696cf19ff74590d0
Ganesh Thota <ganesh_thota@epam.com>
2025-03-02 15:32:40 +0530

    Added navigation properties

--- JobsApi/Models/SendNotification.cs
+++ JobsApi/Models/SendNotification.cs
@@ -14,4 +14,7 @@ public partial class SendNotification
     public string Type { get; set; } = null!;
 
     public bool IsNotificationSent { get; set; }
+    // Navigation properties
+    public Job Job { get; set; }
+    public User User { get; set; }
 }
